6eea229190290c764ed7cfc1a0143292672ca382
XJJ20201060337 <xjj18107971497@163.com>
2022-07-03 15:29:19 +0800

    first commit

---
+++ chapter_03_01/android/app/src/main/kotlin/com/example/chapter_03_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chapter_03_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ chapter_07_02/android/app/src/main/kotlin/com/example/chapter_07_02/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chapter_07_02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
